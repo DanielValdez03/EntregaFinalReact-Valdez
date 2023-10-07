e4aed32e39a38b793e19f678f6b842e52caa09cc
Daniel Valdez <valdezdaniel1612@gmail.com>
2023-10-06 23:23:00 -0300

    arreglo de links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import CartWidget from './CartWidget'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand text-white" to="/">GoldenBoy Camisetas</Link>
+    <NavLink className="navbar-brand text-white" to="/">GoldenBoy Camisetas</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link text-white" aria-current="page" to="/">Productos</Link>
+          <NavLink className="nav-link text-white" aria-current="page" to="/productos">Productos</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link text-white" to="/productos/remeras">Remeras</Link>
+          <NavLink className="nav-link text-white" to="/productos/remeras">Remeras</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link text-white" to="/productos/buzos">Buzos</Link>
+          <NavLink className="nav-link text-white" to="/productos/buzos">Buzos</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link text-white" to="/productos/pantalones">Pantalones</Link>
+          <NavLink className="nav-link text-white" to="/productos/pantalones">Pantalones</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link text-white" to="/productos/medias">Medias</Link>
+          <NavLink className="nav-link text-white" to="/productos/medias">Medias</NavLink>
         </li>
       </ul>
     </div>
